5b225651db33b1372ee39266de4dc2c41cb91b63
Kallinikos <84861402+Kall-K@users.noreply.github.com>
2024-09-19 15:48:49 +0300

    Update queries.py

--- queries.py
+++ queries.py
@@ -36,37 +36,44 @@ def main():
     start_time = input('Give Start Time (YYYY-MM-DD hh:mm:ss): ')
     end_time = input('Give End Time (YYYY-MM-DD hh:mm:ss): ')
     query = input('Select query (1 or 2 or 3): ')
+    
+    # Set values manually
+    # start_time="2024-09-19 15:35:03"
+    # end_time="2024-09-08 13:57:04"
+    # query=3
 
     client = MongoClient('localhost', 27017)
     db = client['traffic']
     collection = db['processed_data']
 
     result = 0
+    try:
+        # Execute the aggregation
+        if int(query) == 1:
+            result = collection.aggregate(min_vehicles(start_time, end_time))
+            result = loads(dumps(result))
+            r = result[0]
+            links = ", ".join(r['links'])
+            print(f"Minimum Number of Vehicles = {r['_id']}\nLinks = {links}")
+        elif int(query) == 2:
+            result = collection.aggregate(max_avg_speed(start_time, end_time))
+            result = loads(dumps(result))
+            r = result[0]
+            links = ", ".join(r['links'])
+            print(f"Maximum Avg Speed = {r['_id']}\nLinks = {links}")
+        elif int(query) == 3:
+            collection = db['raw_data']
+            result = collection.aggregate(max_route(start_time, end_time))
+            result = loads(dumps(result))
+            r = result[0]
+            cars = [c for c in r['names']]
+            routes = [list(dict.fromkeys(x)) for x in r['routes']]
+            print(f"Maximum Distance = {r['_id']}")
+            for i in range(len(routes)):
+                links = ", ".join(routes[i])
+                print(f"carID: {cars[i]}   Max Route: {links}")
+    except:
+        print('No Result Found')
 
-    # Execute the aggregation
-    if int(query) == 1:
-        result = collection.aggregate(min_vehicles(start_time, end_time))
-        result = loads(dumps(result))
-        r = result[0]
-        links = ", ".join(r['links'])
-        print(f"Minimum Number of Vehicles = {r['_id']}\nLinks = {links}")
-    elif int(query) == 2:
-        result = collection.aggregate(max_avg_speed(start_time, end_time))
-        result = loads(dumps(result))
-        r = result[0]
-        links = ", ".join(r['links'])
-        print(f"Maximum Avg Speed = {r['_id']}\nLinks = {links}")
-    elif int(query) == 3:
-        collection = db['raw_data']
-        result = collection.aggregate(max_route(start_time, end_time))
-        result = loads(dumps(result))
-        r = result[0]
-        cars = [c for c in r['names']]
-        routes = [list(dict.fromkeys(x)) for x in r['routes']]
-        print(f"Maximum Distance = {r['_id']}")
-        for i in range(len(routes)):
-            links = ", ".join(routes[i])
-            print(f"carID: {cars[i]}   Max Route: {links}")
-    
 if __name__ == "__main__":
     main()
